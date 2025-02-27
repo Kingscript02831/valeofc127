fea7f75c40fb1529fbb0ea51f9ccc43d4674aa0a
Vinix <marcosvinihhh2003@gmail.com>
2025-02-27 13:00:20 -0300

    Update Chat.tsx

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,8 +1,8 @@
 
 import { useState, useEffect, useRef } from "react";
 import { useSearchParams } from "react-router-dom";
-import { ChatHeader } from "../components/chat/ChatHeader";
-import { ChatInput } from "../components/chat/ChatInput";
+import { ChatHeader } from "@/components/chat/ChatHeader";
+import { ChatInput } from "@/components/chat/ChatInput";
 import { Message, MessageType } from "../components/chat/Message";
 import { v4 as uuidv4 } from "uuid";
 
